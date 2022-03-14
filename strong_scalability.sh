b90b042e274f233c03c3cd395c94c80cc412295e
SilvioBaratto <silviobaratto@xps-13-9310.homenet.telecomitalia.it>
2022-03-14 16:02:47 +0100

    final changes

--- strong_scalability.sh
+++ strong_scalability.sh
@@ -6,18 +6,18 @@ cd ..
 
 cd bin
 
-for i in 2 4
+for i in 2 4 8 16 32 64
 do
     mpirun -np ${i} tree_mpi.x >> ../time/mpi.out
 done
 
-for i in 2 4
+for i in 2 4 8 16 32 64
 do
     export OMP_NUM_THREADS=${i} 
     ./tree_omp.x >> ../time/omp.out
 done
 
-for i in 2 4
+for i in 2 4 8 16 32 64
 do
     export OMP_NUM_THREADS=${i}
     ./tree_serial.x >> ../time/serial.out
