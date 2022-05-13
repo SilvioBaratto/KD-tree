563e5039d65e36e43c66a14e8fa355384027d77d
Silvio Baratto <sbaratto@login.hpc>
2022-05-13 18:33:42 +0200

    weak final time

--- weak_scalability.sh
+++ weak_scalability.sh
@@ -2,16 +2,10 @@
 
 module load openmpi-4.1.1+gnu-9.3.0
 
-make all src=mpi
-make all src=omp
-make all src=serial
+make src=mpi
 
-cd datasets/float/
-
-c++ write_data.cpp -o write_data.x
-echo "writing data"
-./write_data.x
-echo "finish writing datasets"
+cd time/weak/
+rm mpi.csv
 
 cd ..
 cd ..
@@ -23,23 +17,5 @@ declare -a nprocs=(1 2 4 8 16)
 for i in "${nprocs[@]}"
 do
     echo "executing mpi with: ${i}"
-    mpirun -np ${i} tree_mpi.x ../datasets/float/dataset.csv >> ../time/weak/mpi.out
+    mpirun -np ${i} tree_mpi.x ../datasets/float/dataset.csv >> ../time/weak/mpi.csv
 done
-
-for i in "${nprocs[@]}"
-do
-    echo "executing omp with: ${i}"
-    export OMP_NUM_THREADS=${i} 
-    ./tree_omp.x ../datasets/float/dataset.csv >> ../time/weak/omp.out 
-done
-
-for i in "${nprocs[@]}"
-do
-    echo "executing serial with: ${i}"
-    export OMP_NUM_THREADS=${i}
-    ./tree_serial.x ../datasets/float/dataset.csv >> ../time/weak/serial.out
-done
-
-cd ..
-cd datasets/float/
-rm *.csv
